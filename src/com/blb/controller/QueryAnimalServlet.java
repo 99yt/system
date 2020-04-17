570aa65a5f560a0dfbf4fd72492abd096b892be1
yang <825025679@qq.com>
2020-04-17 15:00:16 +0800

    开发别的功能

--- src/com/blb/controller/QueryAnimalServlet.java
+++ src/com/blb/controller/QueryAnimalServlet.java
@@ -22,7 +22,7 @@ public class QueryAnimalServlet extends HttpServlet {
         //2、调用模型层代码，处理请求
         AnimalService service=new AnimalService();
         List<Animal> animals=null;
-        System.out.println("333");
+        System.out.println("66666");
         try {
             animals=service.queryAllAnimal();
         } catch (SQLException e) {
